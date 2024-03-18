f4a852d2efffbeda2d583682eb7a41394872f675
Soumen <soumen1696@gmail.com>
2024-03-18 16:50:57 +0100

    Update visualizer.py
    Adding visualizer Logic

--- dataprocprofiler/visualizer.py
+++ dataprocprofiler/visualizer.py
@@ -1,1 +1,11 @@
 
+import matplotlib.pyplot as plt
+
+def plot_execution_times(data):
+    functions, times = zip(*sorted(data.items(), key=lambda item: item[1]))
+    plt.bar(functions, times)
+    plt.xlabel('Function')
+    plt.ylabel('Execution Time (s)')
+    plt.title('Function Execution Times')
+    plt.xticks(rotation=45)
+    plt.show()
